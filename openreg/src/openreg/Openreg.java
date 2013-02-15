e8ff0552b64db493142d95ac71b9ff89e33b8d70
pitiz29a@gmail.com <pitiz29a@gmail.com@37c36fcb-528a-1a31-8e57-5a1d338e5082>
2013-02-15 15:19:11 +0000

    Cleanup

--- openreg/src/openreg/Openreg.java
+++ openreg/src/openreg/Openreg.java
@@ -10,10 +10,12 @@ public class Openreg {
 		try {
 			
 			ArrayList<Row> rows = DatabaseTools.getQueryResult(
-					"SELECT * FROM student WHERE class_id=? AND ability_description_id=?", 1, 1);
+					"SELECT * FROM student " +
+					"INNER JOIN ability_description ad ON ability_description_id = ad.id " +
+					"WHERE class_id = ? AND ability_description_id = ?", 1, 1);
 			
 			for(Row row : rows) {
-				System.out.println(row.getValue("name"));
+				System.out.println(row.getValue("name") + " " + row.getValue("description"));
 			}
 			
 		} catch (Exception e) {
