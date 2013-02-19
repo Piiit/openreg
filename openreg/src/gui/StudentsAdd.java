07aca313fff20c3c1ab64ee228b878dbe4667456
pitiz29a@gmail.com <pitiz29a@gmail.com@37c36fcb-528a-1a31-8e57-5a1d338e5082>
2013-02-19 16:14:26 +0000

    Working on students and classes Widgets and data structures

---
+++ openreg/src/gui/StudentsAdd.java
@@ -0,0 +1,51 @@
+package gui;
+
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+
+public class StudentsAdd extends Dialog {
+
+	protected Object result;
+	protected Shell shlAddStudent;
+
+	/**
+	 * Create the dialog.
+	 * @param parent
+	 * @param style
+	 */
+	public StudentsAdd(Shell parent, int style) {
+		super(parent, style);
+		setText("SWT Dialog");
+	}
+
+	/**
+	 * Open the dialog.
+	 * @return the result
+	 */
+	public Object open() {
+		createContents();
+		shlAddStudent.open();
+		shlAddStudent.layout();
+		Display display = getParent().getDisplay();
+		while (!shlAddStudent.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	private void createContents() {
+		shlAddStudent = new Shell(getParent(), SWT.DIALOG_TRIM | SWT.MAX | SWT.RESIZE | SWT.APPLICATION_MODAL);
+		shlAddStudent.setSize(450, 300);
+		shlAddStudent.setText("Add student");
+		shlAddStudent.setLayout(new GridLayout(1, false));
+
+	}
+}
