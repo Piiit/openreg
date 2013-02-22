65c666c8dcf8e895feb451ea10f4542fe8389642
pitiz29a@gmail.com <pitiz29a@gmail.com@37c36fcb-528a-1a31-8e57-5a1d338e5082>
2013-02-22 12:53:22 +0000

    Initial import

---
+++ trunk/openreg/resources/clean.sql
@@ -0,0 +1,24 @@
+﻿
+/*
+Peter Moser peter.moser@stud-inf.unibz.it
+Valentin Huber valentin.huber@stud-inf.unibz.it
+Johannes Ganthaler johannes.ganthaler@stud-inf.unibz.it
+
+ds_group2
+*/
+
+
+DROP TABLE IF EXISTS address CASCADE;
+DROP TABLE IF EXISTS class CASCADE;
+DROP TABLE IF EXISTS teacher CASCADE;
+DROP TABLE IF EXISTS ability_description CASCADE;
+DROP TABLE IF EXISTS student CASCADE;
+DROP TABLE IF EXISTS course CASCADE;
+DROP TABLE IF EXISTS teacher_class_course CASCADE;
+DROP TABLE IF EXISTS assessment_type CASCADE;
+DROP TABLE IF EXISTS topic CASCADE;
+DROP TABLE IF EXISTS assessment CASCADE;
+DROP TABLE IF EXISTS weighted_assessment CASCADE;
+DROP TABLE IF EXISTS mark_type CASCADE;
+DROP TABLE IF EXISTS mark CASCADE;
+DROP TABLE IF EXISTS assessment_student CASCADE;
