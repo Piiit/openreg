e0bc5bf2e4027240052fe7a92d91176f6ca682b8
pitiz29a@gmail.com <pitiz29a@gmail.com@37c36fcb-528a-1a31-8e57-5a1d338e5082>
2013-02-28 09:45:01 +0000

    Showing Filter setting inside StudentsModule

---
+++ trunk/openreg/src/testing/gui/ClassesModuleTest.java
@@ -0,0 +1,44 @@
+package testing.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClassesModuleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateContent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReloadData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReloadDataObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+}
