ab11712d720c946493c3fe9feb4a99e931bf745d
johannes.ganthaler@gmail.com <johannes.ganthaler@gmail.com@37c36fcb-528a-1a31-8e57-5a1d338e5082>
2013-03-14 22:17:43 +0000

    Modified Grade Query

--- openreg/src/gui/dialogs/GradeDialog.java
+++ openreg/src/gui/dialogs/GradeDialog.java
@@ -1,6 +1,7 @@
 package gui.dialogs;
 
 import java.util.ArrayList;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -13,13 +14,11 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TableItem;
 import database.Row;
-import database.query.ClassQuery;
 import database.query.GradeQuery;
-import database.query.MarkQuery;
 import database.query.MarkTypeQuery;
+import database.query.StudentQuery;
 import gui.GuiDialog;
 import gui.GuiTools;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -30,8 +29,10 @@ public class GradeDialog extends GuiDialog {
 
 	protected Object result;
 	protected Shell shlDialog;
-	protected Row loadedData;
 	private Table table;
+	private Combo combo;
+	private Combo comboName;
+	protected Row loadedData;
 	
 	public GradeDialog(Shell parent) {
 		super(parent);
@@ -59,9 +60,15 @@ public class GradeDialog extends GuiDialog {
 	 */
 	private void createContents() {
 		shlDialog = new Shell(getParent(), SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
-		shlDialog.setSize(346, 400);
-		shlDialog.setText("Add a new mark");
+		shlDialog.setSize(800, 500);
+		shlDialog.setText("Grade an assessments");
 		shlDialog.setLayout(new FormLayout());
+		
+		Label lblDescription = new Label(shlDialog, SWT.NONE);
+		FormData fd_lblDescription = new FormData();
+		fd_lblDescription.left = new FormAttachment(0, 10);
+		lblDescription.setLayoutData(fd_lblDescription);
+		lblDescription.setText("Select a main assessment *");
 		
 		Label label = new Label(shlDialog, SWT.SEPARATOR | SWT.HORIZONTAL);
 		FormData fd_label = new FormData();
@@ -69,85 +76,146 @@ public class GradeDialog extends GuiDialog {
 		fd_label.right = new FormAttachment(100, -10);
 		label.setLayoutData(fd_label);
 		
-		Button btnSave = new Button(shlDialog, SWT.NONE);
-		fd_label.bottom = new FormAttachment(btnSave, -17);
-		btnSave.addSelectionListener(new SelectionAdapter() {
+		Button btnOk = new Button(shlDialog, SWT.NONE);
+		fd_label.bottom = new FormAttachment(btnOk, -1);
+		btnOk.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
 				cancel();
 			}
 		});
 		FormData fd_btnSave = new FormData();
-		fd_btnSave.top = new FormAttachment(0, 340);
-		fd_btnSave.right = new FormAttachment(100, -14);
-		btnSave.setLayoutData(fd_btnSave);
-		btnSave.setText("Done");
-		fd_btnSave.left = new FormAttachment(0, 258);
+		fd_btnSave.top = new FormAttachment(0, 442);
+		fd_btnSave.right = new FormAttachment(100, -10);
+		fd_btnSave.left = new FormAttachment(0, 709);
+		btnOk.setLayoutData(fd_btnSave);
+		btnOk.setText("OK");
 		
-		table = new Table(shlDialog, SWT.BORDER | SWT.CHECK | SWT.FULL_SELECTION | SWT.HIDE_SELECTION);
-		fd_label.top = new FormAttachment(table, 27);
+		Label lblMandatoryFields = new Label(shlDialog, SWT.NONE);
+		FormData fd_lblMandatoryFields = new FormData();
+		fd_lblMandatoryFields.bottom = new FormAttachment(100, -10);
+		fd_lblMandatoryFields.left = new FormAttachment(0, 10);
+		lblMandatoryFields.setLayoutData(fd_lblMandatoryFields);
+		lblMandatoryFields.setText("* Mandatory Fields");
+		
+		combo = new Combo(shlDialog, SWT.READ_ONLY);
+		fd_lblDescription.bottom = new FormAttachment(combo, -6);
+		combo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				updateSubAssessmentTable();
+			}
+		});
+		FormData fd_combo = new FormData();
+		fd_combo.top = new FormAttachment(0, 81);
+		fd_combo.right = new FormAttachment(100, -418);
+		fd_combo.left = new FormAttachment(0, 10);
+		combo.setLayoutData(fd_combo);
+		
+		table = new Table(shlDialog, SWT.BORDER | SWT.CHECK | SWT.FULL_SELECTION);
+		fd_label.top = new FormAttachment(table, 57);
 		table.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDoubleClick(MouseEvent arg0) {
-				GradeSubDialog dialog = new GradeSubDialog(shlDialog);
+				AssessmentMarkDialog dialog = new AssessmentMarkDialog(shlDialog);
 				try {
 					TableItem ti = table.getItem(table.getSelectionIndex());
-					dialog.loadData(ti.getData());
+					dialog.loadData(GuiTools.getIdFromCombo(combo), ti.getData(), GuiTools.getIdFromCombo(comboName));
 				} catch (Exception e) {
 					e.printStackTrace();
 					GuiTools.showMessageBox(shlDialog, e.getMessage());
 				}
 				dialog.open();
-				updateStudentsTable();
+				updateSubAssessmentTable();
 			}
 		});
 		FormData fd_table = new FormData();
-		fd_table.top = new FormAttachment(0, 10);
+		fd_table.bottom = new FormAttachment(100, -101);
+		fd_table.top = new FormAttachment(combo, 12);
+		fd_table.right = new FormAttachment(label, 0, SWT.RIGHT);
 		fd_table.left = new FormAttachment(0, 10);
-		fd_table.right = new FormAttachment(100, -14);
 		table.setLayoutData(fd_table);
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
 		
-		TableColumn tblclmnName = new TableColumn(table, SWT.NONE);
-		tblclmnName.setWidth(100);
-		tblclmnName.setText("Name");
+		Label lblSubassessment = new Label(shlDialog, SWT.NONE);
 		
-		TableColumn tblclmnSurname = new TableColumn(table, SWT.NONE);
-		tblclmnSurname.setWidth(100);
-		tblclmnSurname.setText("Surname");
-		fd_table.bottom = new FormAttachment(100, -78);
+		TableColumn tblclmnName = new TableColumn(table, SWT.NONE);
+		tblclmnName.setWidth(450);
+		tblclmnName.setText("Description");
 		
 		TableColumn tblclmnMark = new TableColumn(table, SWT.NONE);
-		tblclmnMark.setWidth(50);
+		tblclmnMark.setWidth(100);
 		tblclmnMark.setText("Mark");
+		FormData fd_lblSubassessment = new FormData();
+		fd_lblSubassessment.left = new FormAttachment(lblDescription, 0, SWT.LEFT);
+		lblSubassessment.setLayoutData(fd_lblSubassessment);
+		lblSubassessment.setText("Student Name");
 		
+		comboName = new Combo(shlDialog, SWT.READ_ONLY);
+		fd_lblSubassessment.top = new FormAttachment(comboName, 3, SWT.TOP);
+		comboName.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				updateNameField(GuiTools.getIdFromCombo(comboName));
+			}
+		});
+		FormData fd_combo_2 = new FormData();
+		fd_combo_2.bottom = new FormAttachment(lblDescription, -17);
+		fd_combo_2.right = new FormAttachment(combo, 0, SWT.RIGHT);
+		fd_combo_2.left = new FormAttachment(0, 104);
+		comboName.setLayoutData(fd_combo_2);
+
 		update();
 	}
-
+	
 	@Override
 	public void loadData(Object data) throws Exception {
-		ArrayList<Row> ab = GradeQuery.getStudentDataset(data);
-		if(ab.size() == 0) {
-			throw new Exception("No assessment ID " + data.toString() + " found.");
+		ArrayList<Row> query = GradeQuery.getDataset(data);
+		if(query.size() == 0) {
+			throw new Exception("No student with ID " + data.toString() + " found.");
 		}
-		loadedData = ab.get(0); 
+		loadedData = query.get(0); 
 	}
 
 	@Override
 	public void store() {
 	}
-	
 
 	@Override
 	public void update() {
 		try {
 			
 			if (loadedData != null){
-				
-				shlDialog.setText("Select the student you want to grade");
-				updateStudentsTable();
-				
+				updateNameField(loadedData.getValueAsLong("student_id"));
+				updateMainAssessmentField(loadedData.getValueAsLong("assessment_id"));
+				updateSubAssessmentTable();
+			} else {
+				updateNameField(null);
+				updateMainAssessmentField(null);
+				updateSubAssessmentTable();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			GuiTools.showMessageBox(shlDialog, e.getMessage());
+		}
+	}
+	
+	private void updateSubAssessmentTable() {
+		table.removeAll();
+		Long studentId = GuiTools.getIdFromCombo(comboName);
+		Long assessmentId = GuiTools.getIdFromCombo(combo);
+		if(studentId == null) {
+			return; //nothing selected... nothing to do!
+		}
+		try {
+			for(Row row: GradeQuery.getSubAssessments(assessmentId, studentId)) {
+				TableItem tableItem = new TableItem(table, SWT.NONE);
+				tableItem.setData(row.getValueAsLong("student_id"));
+				tableItem.setText(new String[] {
+						row.getValueAsString("description"),
+						row.getValueAsString("mark")
+						});
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -155,23 +223,52 @@ public class GradeDialog extends GuiDialog {
 		}
 	}
 
-	private void updateStudentsTable() {
-		table.removeAll();
-		ArrayList<Row> students = null;
+	private void updateNameField(Long id) {
 		try {
-			students = GradeQuery.getStudentDataset(loadedData.getValueAsLong("id"));
+			comboName.removeAll();
+			int i = 0;
+			for(Row row : StudentQuery.getFullDataset()) {
+				String assessmentString = row.getValueAsStringNotNull("name") + 
+						" " + row.getValueAsStringNotNull("surname");
+				comboName.add(assessmentString);
+				comboName.setData(assessmentString, row.getValueAsLong("student_id"));
+				if(row.getValueAsLong("student_id").equals(id)) {
+					comboName.select(i);
+				}
+				i++;
+			}
+			if(id == null) {
+				comboName.select(0);
+			}
+			updateMainAssessmentField(null);
+			updateSubAssessmentTable();
 		} catch (Exception e) {
 			e.printStackTrace();
 			GuiTools.showMessageBox(shlDialog, e.getMessage());
 		}
-		for(Row stud : students) {
-			TableItem ti = new TableItem(table, SWT.NONE);
-			ti.setData(stud.getValueAsLong("id"));
-			ti.setText(new String[] {
-					stud.getValueAsStringNotNull("name"), 
-					stud.getValueAsStringNotNull("surname"),
-					stud.getValueAsStringNotNull("representation")
-			});
+	}
+	
+	private void updateMainAssessmentField(Long id) {
+		try {
+			combo.removeAll();
+			int i = 0;
+			for(Row row : GradeQuery.getFullDataset1(GuiTools.getIdFromCombo(comboName))) {
+				String assessmentString = row.getValueAsStringNotNull("assessment_description");// + 
+//						" (" + row.getValueAsStringNotNull("assessment_type_description") + ")";
+				combo.add(assessmentString);
+				combo.setData(assessmentString, row.getValueAsLong("assessment_id"));
+				if(row.getValueAsLong("assessment_id").equals(id)) {
+					combo.select(i);
+				}
+				i++;
+			}
+			if(id == null) {
+				combo.select(0);
+			}
+			updateSubAssessmentTable();
+		} catch (Exception e) {
+			e.printStackTrace();
+			GuiTools.showMessageBox(shlDialog, e.getMessage());
 		}
 	}
 
